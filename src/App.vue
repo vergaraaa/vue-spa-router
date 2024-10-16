50472be06282849314b676cdfcb6809ec7ed1f0a
Edgar Vergara <edgarvm13@hotmail.com>
2024-10-16 13:55:29 +0200

    💄 structure and components of app

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,43 @@
-<script setup lang="ts">
-import { RouterView } from 'vue-router';
-</script>
-
 <template>
-  <nav>
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/features">Features</RouterLink>
-    <RouterLink to="/pricing">Pricing</RouterLink>
-    <RouterLink to="/contact">Contact</RouterLink>
-  </nav>
+  <div class="flex flex-col h-screen">
+    <!-- Header -->
+    <header
+      class="flex items-center h-14 px-4 border-b border-gray-300 sm:h-16 md:px-6 lg:px-8"
+    >
+      <div>
+        <a class="flex items-center gap-2 font-semibold" href="#">
+          <img
+            alt="Vue logo"
+            class="logo"
+            src="@/assets/logo.svg"
+            width="40"
+            height="40"
+          />
+        </a>
+      </div>
+      <nav class="ml-auto space-x-4 flex items-center h-10 sm:space-x-6">
+        <RouterLink :to="{ name: 'home' }"> Home </RouterLink>
+        <RouterLink to="/features"> Features </RouterLink>
+        <RouterLink to="/pricing"> Pricing </RouterLink>
+        <RouterLink to="/contact"> Contact </RouterLink>
+      </nav>
+    </header>
+    <!-- Fin Header -->
 
-  <main class="bg-slate-100">
-    <RouterView />
-  </main>
+    <!-- Main -->
+    <main class="flex-1 flex items-center justify-center">
+      <RouterView />
+    </main>
+    <!-- End Main -->
+
+    <!-- Footer -->
+    <footer
+      class="flex items-center h-14 px-4 border-t border-gray-300 sm:h-16 md:px-6 lg:px-8"
+    >
+      <p class="flex-1 text-sm text-gray-500 text-center">
+        © {{ new Date().getFullYear() }} Acme Corporation. Rights reserved
+      </p>
+    </footer>
+    <!-- End Footer -->
+  </div>
 </template>
--- src/modules/landing/pages/HomePage.vue
+++ src/modules/landing/pages/HomePage.vue
@@ -1,3 +1,10 @@
 <template>
-  <h1>Home Page</h1>
+  <div class="text-center">
+    <h1 class="text-4xl font-bold tracking-tighter sm:text-5xl">
+      Welcome to our website
+    </h1>
+    <p class="mx-auto max-w-[600px] text-gray-500 md:text-xl">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    </p>
+  </div>
 </template>
